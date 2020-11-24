88ec83ae9d4d1f9e26f475678a08daf5cc65f5ac
Ivanov Peter <peterivan5555@gmail.com>
2020-11-24 22:56:25 +0700

    fix_problems

--- gui.py
+++ gui.py
@@ -5,15 +5,16 @@ from Assistant import *
 
 class App(QWidget):
     def __init__(self):
+        super().__init__()
         self.Pirs = Assistant()
         self.ui = uic.loadUi("gui_pirs.ui")
         self.ui.show()
 
-        self.ui.start.clicked.connect(self.Pirs.voice_activation())
+        self.ui.start.clicked.connect(self.Pirs.voice_activation)
 
 
 if __name__ == '__main__':
-    Assistant.greeting()
     app = QApplication(sys.argv)
     ex = App()
+    Assistant.greeting()
     app.exec()
