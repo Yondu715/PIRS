cdf692f5cecadf699d651424d6ded1858e21cc87
Ivanov Peter <peterivan5555@gmail.com>
2020-11-24 22:45:30 +0700

    first_connect

--- gui.py
+++ gui.py
@@ -1,16 +1,19 @@
-
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget
 from PyQt5 import uic
+from Assistant import *
 
 class App(QWidget):
     def __init__(self):
+        self.Pirs = Assistant()
         self.ui = uic.loadUi("gui_pirs.ui")
         self.ui.show()
-    def set(self):
-        self.ui.start.clicked.connect()
+
+        self.ui.start.clicked.connect(self.Pirs.voice_activation())
+
 
 if __name__ == '__main__':
+    Assistant.greeting()
     app = QApplication(sys.argv)
     ex = App()
     app.exec()
