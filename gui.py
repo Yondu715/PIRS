d50f7308742a9997f9c5fdc2b09433e4b4e1d9cf
Ivanov Peter <peterivan5555@gmail.com>
2020-11-25 00:31:11 +0700

    Pirs_version_1.0

--- gui.py
+++ gui.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QMessageBox
-from PyQt5 import uic, QtCore
+from PyQt5 import uic, QtGui
+from PyQt5.QtCore import Qt
 from Assistant import *
 
 class App(QWidget):
@@ -12,8 +13,10 @@ class App(QWidget):
         self.Pirs = Assistant()
         self.Pirs.moveToThread(self.thread)
         self.ui.start.clicked.connect(self.Pirs.voice_activation)
+        self.ui.start.clicked.connect(self.fix_label)
         self.thread.start()
         self.ui.pushButton.clicked.connect(self.check_microfone)
+
 
     def check_microfone(self):
         microphone_list = pyaudio.PyAudio().get_device_count()
@@ -24,7 +27,11 @@ class App(QWidget):
             descriprion = microphone_list
         QMessageBox.about(self,'Микрофоны в системе',str(descriprion))
 
-
+    def fix_label(self):
+        self.ui.label_2.setText("Pirs на связи")
+        self.ui.label_2.setAlignment(Qt.AlignCenter)
+        self.ui.label_2.setFont(QtGui.QFont("MS Shell Dlg 2", 24))
+        self.ui.frame.setStyleSheet("background-color: rgb(152, 251, 152)")
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
