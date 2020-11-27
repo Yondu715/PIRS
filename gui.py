78f66e061e839e67d2c0e397579c1e50ba268ce7
Ivanov Peter <peterivan5555@gmail.com>
2020-11-27 23:56:13 +0700

    _

--- gui.py
+++ gui.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QMessageBox
+from PyQt5.QtWidgets import QApplication, QWidget, QMessageBox, QMainWindow
 from PyQt5 import uic, QtGui
 from PyQt5.QtCore import Qt
 from Assistant import *
@@ -15,8 +15,12 @@ class App(QWidget):
         self.ui.start.clicked.connect(self.Pirs.voice_activation)
         self.ui.start.clicked.connect(self.fix_label)
         self.thread.start()
-        self.ui.pushButton.clicked.connect(self.check_microfone)
+        self.ui.pushButton.clicked.connect(self.Dialog)
 
+    def Dialog(self):
+        self.ui = uic.loadUi("settings.ui")
+        self.ui.show()
+        self.thread = QtCore.QThread()
 
     def check_microfone(self):
         microphone_list = pyaudio.PyAudio().get_device_count()
@@ -24,14 +28,15 @@ class App(QWidget):
         if microphone_list == []:
             description = 'Микрофоны не были обноружены в системе'
         else:
-            descriprion = microphone_list
-        QMessageBox.about(self,'Микрофоны в системе',str(descriprion))
+            description = 'Микрофон подключен'
+        QMessageBox.about(self, 'Микрофоны в системе', str(description))
 
     def fix_label(self):
         self.ui.label_2.setText("Pirs на связи")
         self.ui.label_2.setAlignment(Qt.AlignCenter)
         self.ui.label_2.setFont(QtGui.QFont("MS Shell Dlg 2", 24))
         self.ui.frame.setStyleSheet("background-color: rgb(152, 251, 152)")
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
